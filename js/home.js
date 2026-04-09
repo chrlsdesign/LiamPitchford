d6c7cfe1150b120d70c240d985163186ee8cb4b9
chrlsdesign <contact@chrls.design>
2026-04-09 22:49:18 +0700

    'update'

--- js/home.js
+++ js/home.js
@@ -162,7 +162,7 @@ export function initHome({
 
     const introObs = onScroll({
       target: scrollThres,
-      enter: "center center",
+      enter: "center top",
       leave: "top bottom",
       onLeaveForward: function handler(self) {
         self.revert();
