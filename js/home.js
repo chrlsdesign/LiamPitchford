d1cbe970fc2290a2ca725ea924f88341c35c898c
chrlsdesign <contact@chrls.design>
2026-04-11 01:59:37 +0700

    'update'

--- js/home.js
+++ js/home.js
@@ -99,13 +99,16 @@ function initScrollReveal(cubicEase) {
   if (!originalList) return;
   const items = originalList.querySelectorAll(".home_item");
 
+  let translate = new DOMMatrix(originalList.style.transform).m42;
+
   items.forEach((item) => {
     const observer = onScroll({
       target: item,
       repeat: false,
       debug: true,
+      enter: `${translate}vh`,
       onUpdate: () => {
-        console.log(originalList.style.transform);
+        console.log(translate);
       },
       onEnter: () => {
         if (played.has(item)) return;
